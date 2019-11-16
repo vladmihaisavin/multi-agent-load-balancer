fb5d2fe7550162f424973bfe6691bc7c8714df17
Vlad's Mac <savin.vladmihai@gmail.com>
2019-11-16 11:25:00 +0200

    Initialized .NET console app

---
+++ multi-agent-load-balancer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multi_agent_load_balancer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
